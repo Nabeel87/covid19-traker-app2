c4ba65c6635fd9f85a81a1213bc919d743837387
Nabeel87 <nabilsabir786@gmail.com>
2020-08-26 00:27:16 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import NavBar from './components/NavBar';
+import InfoPanel from './components/InfoPanel';
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <InfoPanel />
+    </div>
+  );
+}
+
+export default App;
